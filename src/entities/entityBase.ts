e12d21991a63cbef8055f7058b5fe0e62db627ad
Subho-Arghya <sarghya18@gmail.com>
2022-12-27 11:08:59 +0530

    First commit

---
+++ src/entities/entityBase.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class EntityBase {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column() 
+    creation: Date = new Date()
+
+    @Column()
+    modified: Date = new Date()
+}
